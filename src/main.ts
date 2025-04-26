46958fa0483cb37a5e7e5588dfb92461aee79fdd
Marcos Lopez <marcoslopezgarau@gmail.com>
2025-04-26 11:42:39 +0200

    feat: implement authentication microservice with NATS transport and environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Auth-Microservice-Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
 
-  app.setGlobalPrefix('api');
-
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen();
+  logger.log(`Auth Microservice is running on port ${envs.port}`);
 }
 bootstrap();
