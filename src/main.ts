07cd616b73f31b8c0c2aa104188d940723a7b94b
Marcos Lopez <marcoslopezgarau@gmail.com>
2025-04-26 12:12:38 +0200

    feat: add DTOs for user registration and login, implement validation, and update auth controller methods

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
@@ -14,6 +14,13 @@ async function bootstrap() {
         servers: envs.natsServers,
       },
     },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen();
