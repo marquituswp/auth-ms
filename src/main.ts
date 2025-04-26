eeb6b93f30d90b03a4bc4e8c28107be5567c7207
Marcos Lopez <marcoslopezgarau@gmail.com>
2025-04-26 11:57:39 +0200

    feat: add environment configuration for NATS transport and update import path

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
-import { envs } from './config/envs';
+import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('Auth-Microservice-Main');
